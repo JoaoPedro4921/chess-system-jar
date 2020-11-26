8f6345ef20171bf26cf4b676afb0a22ef049d9b0
JoaoPedro4921 <72887783+JoaoPedro4921@users.noreply.github.com>
2020-11-26 18:33:19 -0300

    Special move Castling

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -2,13 +2,17 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class King extends ChessPiece {
+	
+	private ChessMatch chessMatch;
 
-	public King(Board board, Color color) {
+	public King(Board board, Color color, ChessMatch chessMatch) {
 		super(board, color);
+		this.chessMatch = chessMatch;
 
 	}
 
@@ -20,6 +24,11 @@ public class King extends ChessPiece {
 	private boolean canMove(Position position) {
 		ChessPiece p = (ChessPiece) getBoard().piece(position);
 		return p == null || p.getColor() != getColor();
+	}
+	
+	private boolean testRookCastling(Position position) {
+		ChessPiece p  =(ChessPiece)getBoard().piece(position);
+		return p != null && p instanceof Rook && p.getColor() == getColor() && p.getMoveCount() == 0;
 	}
 
 	@Override
@@ -74,6 +83,32 @@ public class King extends ChessPiece {
 		p.setValues(position.getRow() + 1, position.getColumn() + 1);
 		if (getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// special move castling
+		
+		if(getMoveCount() == 0 && !chessMatch.getCheck()) {
+			// special move castling king size rook 
+			Position posT1 = new Position(position.getRow(), position.getColumn() + 3);
+			if(testRookCastling(posT1)) {
+				Position P1 = new Position(position.getRow(), position.getColumn() + 1);
+				Position P2 = new Position(position.getRow(), position.getColumn() + 2);
+				if(getBoard().piece(P1) == null && getBoard().piece(P2) == null ) {
+					mat[position.getRow()][position.getColumn() + 2] = true;
+				}
+			}
+			
+			// special move castling Queen size rook 
+			Position posT2 = new Position(position.getRow(), position.getColumn() - 4);
+			if(testRookCastling(posT2)) {
+				Position P1 = new Position(position.getRow(), position.getColumn() - 1);
+				Position P2 = new Position(position.getRow(), position.getColumn() - 2);
+				Position P3 = new Position(position.getRow(), position.getColumn() - 3);
+				if(getBoard().piece(P1) == null && getBoard().piece(P2) == null && getBoard().piece(P3) == null ) {
+					mat[position.getRow()][position.getColumn() - 2] = true;
+				}
+			}
+			
 		}
 		return mat;
 	}
--- src/chess/pieces/Queen.java
+++ src/chess/pieces/Queen.java
@@ -103,7 +103,7 @@ public class Queen extends ChessPiece {
 		p.setValues((position.getRow() + 1), position.getColumn() + 1);
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
-			p.setValues(p.getRow() + 1, p.getColumn() - 1);
+			p.setValues(p.getRow() + 1, p.getColumn() + 1);
 		}
 		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
